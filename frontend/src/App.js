295bb73f094093938be1eb09e003a23aa23794db
Полина Белякаева <polinabelakaeva@MBP-Polina.modem.local>
2024-06-20 12:04:17 +0300

    adjust rollbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,35 +9,19 @@ import Signup from "./pages/Signup.jsx";
 import NotFound from "./pages/NotFound.jsx";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { Provider, ErrorBoundary } from "@rollbar/react";
-
-const rollbarConfig = {
-  accessToken: "8010c0a4675e42c78a6030896151eb87",
-  environment: "testenv",
-};
-
-function TestError() {
-  const a = null;
-  return a.hello();
-}
 
 const App = () => {
   return (
-    <Provider config={rollbarConfig}>
-      <ErrorBoundary>
-        <TestError />
-        <div className="d-flex flex-column vh-100 bg-light">
-          <Header />
-          <ToastContainer />
-          <Routes>
-            <Route path={ROUTES.home} element={<Home />} />
-            <Route path={ROUTES.login} element={<Login />} />
-            <Route path={ROUTES.signup} element={<Signup />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </div>
-      </ErrorBoundary>
-    </Provider>
+    <div className="d-flex flex-column vh-100 bg-light">
+      <Header />
+      <ToastContainer />
+      <Routes>
+        <Route path={ROUTES.home} element={<Home />} />
+        <Route path={ROUTES.login} element={<Login />} />
+        <Route path={ROUTES.signup} element={<Signup />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
   );
 };
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,16 +9,28 @@ import "./init18n.jsx";
 import { I18nextProvider } from "react-i18next";
 import { AuthProvider } from "./context/authContext.js";
 import i18next from "i18next";
+import { Provider as RollbarProvider, ErrorBoundary } from "@rollbar/react";
+
+const rollbarConfig = {
+  accessToken: process.env.ROLLBAR_ACCESS_TOKEN,
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+  environment: process.env.ROLLBAR_ENVIRONMENT,
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <Provider store={store}>
-      <I18nextProvider i18n={i18next}>
-        <AuthProvider>
-          <App />
-        </AuthProvider>
-      </I18nextProvider>
-    </Provider>
+    <RollbarProvider config={rollbarConfig}>
+      <ErrorBoundary>
+        <Provider store={store}>
+          <I18nextProvider i18n={i18next}>
+            <AuthProvider>
+              <App />
+            </AuthProvider>
+          </I18nextProvider>
+        </Provider>
+      </ErrorBoundary>
+    </RollbarProvider>
   </BrowserRouter>
 );
