25bc306aff8e304884fed27c85d666442a0d8a72
Полина Белякаева <polinabelakaeva@MBP-Polina.modem.local>
2024-06-21 17:11:11 +0300

    fix eslint errors

--- frontend/src/context/authContext.js
+++ frontend/src/context/authContext.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import React, { createContext, useState } from "react";
 import { useDispatch } from "react-redux";
 import { setCurrentChannel } from "../slices/uiSlice";
 import { useNavigate } from "react-router-dom";
--- frontend/src/locales/ru.js
+++ frontend/src/locales/ru.js
@@ -1,4 +1,4 @@
-export default {
+const ru = {
   translation: {
     header: {
       logout: "Выйти",
@@ -71,3 +71,5 @@ export default {
     },
   },
 };
+
+export default ru;
