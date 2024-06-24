2311727d52797cceef6d2337204bcb78af52c39d
Полина Белякаева <polinabelakaeva@MBP-Polina.modem.local>
2024-06-24 12:32:35 +0300

    add type text message form

--- frontend/src/components/messages/MessageForm.jsx
+++ frontend/src/components/messages/MessageForm.jsx
@@ -18,7 +18,7 @@ const MessageForm = () => {
     const handleSubmit = async (values, { resetForm }) => {
         const { message } = values;
         const cleanMessage = filter.clean(message)
-        const newMessage = { body: cleanMessage, channelId, username: values.username };
+        const newMessage = { body: cleanMessage, channelId, username };
         try {
             const response = await addMessage(newMessage);
             if (response.error?.status === 'FETCH_ERROR') {
@@ -35,7 +35,7 @@ const MessageForm = () => {
     return (
         <div className='mt-auto px-5 py-3'>
             <Formik
-            initialValues={ { message: '', channelId, username, }}
+            initialValues={ { message: '', channelId, username }}
             onSubmit={handleSubmit}
             >
                 {({ handleSubmit, handleChange, values, isSubmitting }) => (
@@ -43,6 +43,7 @@ const MessageForm = () => {
                     <InputGroup hasValidation>
                     <Form.Label className="visually-hidden" htmlFor="newMessage">{t("messages.newMessage")}</Form.Label>
                         <Form.Control
+                        type="text"
                         name="message"
                         id='newMessage'
                         placeholder={t('chat.typeMessage')}
@@ -53,10 +54,10 @@ const MessageForm = () => {
                         value={values.message}
                         />
                         <Button type="submit" variant="outline" className='btn btn-group-vertical' disabled={!values.message || isSubmitting}>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="20" height="20" fill="currentColor">
-                            <path fillRule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"></path>
-                        </svg>
-                        <span className='visually-hidden'>{t('messages.send')}</span>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="20" height="20" fill="currentColor">
+                                <path fillRule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"></path>
+                            </svg>
+                            <span className='visually-hidden'>{t('messages.send')}</span>
                         </Button>
                     </InputGroup>
                 </Form>
