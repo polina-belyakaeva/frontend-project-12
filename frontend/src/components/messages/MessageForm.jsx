bff9d35e3b72de7ac99370d476e5e34167790758
Полина Белякаева <polinabelakaeva@MBP-Polina.modem.local>
2024-06-26 21:50:26 +0300

    add message input autofocus after page reload

--- frontend/src/components/messages/MessageForm.jsx
+++ frontend/src/components/messages/MessageForm.jsx
@@ -49,6 +49,7 @@ const MessageForm = () => {
                             name="message"
                             id='newMessage'
                             ref={inputRef}
+                            autoFocus={true}
                             placeholder={t('chat.typeMessage')}
                             aria-label={t('messages.newMessage')}
                             className='border-0 p-0 ps-2 form-control'
